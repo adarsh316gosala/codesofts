cac132f2f88140babf172aa4dafcd296a9522504
adarsh316gosala <127820943+adarsh316gosala@users.noreply.github.com>
2023-09-18 12:21:14 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,21 @@
+package codesoft.com.intern;
+
+import java.util.Random;
+
+public class index 
+{
+	public static void main(String[] args)
+	{
+		
+   Random random=new Random();
+   int number=random.nextInt(100);
+   if(number>0)
+   {
+	   
+   System.out.println("random numbers from 1 to 100");
+   System.out.println(number);
+		
+	}
+
+}
+}
